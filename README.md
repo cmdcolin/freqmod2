7854ba0bc19f5c7e75458146f692c67a25707b42
Colin <colin.diesh@gmail.com>
2016-05-03 18:56:20 -0500

    Add blessed and speaker libraries

---
+++ README.md
@@ -0,0 +1,7 @@
+# freqmod2
+
+
+## Usage
+
+
+    node freqmod.js|play -c 1 -r 44100 -b 16 -q -t raw -e signed-integer -
--- freqmod.js
+++ freqmod.js
@@ -1,10 +1,71 @@
-// Usage node freqmod.js|play -c 1 -r 44100 -b 16 -q -t raw -e signed-integer -
+var blessed = require('blessed');
+var Speaker = require('speaker');
+var Readable = require('stream').Readable;
 
-var buflen = 200000;
-var buf = new Uint16Array(buflen);
+var rs = new Readable;
 
-for(var i=0;i<buflen;i++) {
-    buf[i]=Math.sin(i/20)*1000;
+// Create the Speaker instance
+var speaker = new Speaker({
+  channels: 2,          // 2 channels
+  bitDepth: 16,         // 16-bit samples
+  sampleRate: 44100     // 44,100 Hz sample rate
+});
+
+
+// Create a screen object.
+var screen = blessed.screen({
+    smartCSR: true
+});
+
+screen.title = 'freqmod2';
+
+// Create a box perfectly centered horizontally and vertically.
+var box = blessed.box({
+    content: 'freqmod2',
+    tags: true,
+    border: {
+        type: 'line'
+    },
+    style: {
+        border: {
+            fg: '#f0f0f0'
+        }
+    }
+});
+
+// Append our box to the screen.
+screen.append(box);
+
+// If box is focused, handle `enter`/`return` and give us some more content.
+screen.key('q', function(ch, key) { freq *= 2; });
+screen.key('w', function(ch, key) { freq /= 2; });
+
+screen.key('p', function(ch) {
+    rs.pipe(speaker);
+});
+
+// Quit on Escape, q, or Control-C.
+screen.key(['escape', 'q', 'C-c'], function(ch, key) {
+    return process.exit(0);
+});
+
+// Focus our element.
+box.focus();
+
+// Render the screen.
+screen.render();
+
+function fillBuf(i) {
+    var buflen = 1024;
+    var A = 2000;
+
+    var buf = new Uint16Array(buflen);
+    for(var j = 0; j < buflen; j++) {
+        buf[j] = Math.sin((i+j)/40)*A;
+    }
+    rs.push(buf.toString());
+    setTimeout(fillBuf, 10, i+j);
 }
 
-process.stdout.write(new Buffer(buf.buffer),'binary');
+fillBuf(0);
+
