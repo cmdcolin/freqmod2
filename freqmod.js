66cabd6f1e473adbe443d6ce4fd2ce1bd1ae9b18
Colin <colin.diesh@gmail.com>
2016-05-03 20:32:20 -0500

    Rewrite with a custom audio stream generator

--- freqmod.js
+++ freqmod.js
@@ -1,71 +1,73 @@
 var blessed = require('blessed');
-var Speaker = require('speaker');
+var fs = require('fs');
+var inherits = require('util').inherits;
 var Readable = require('stream').Readable;
+var Speaker = require('speaker');
 
-var rs = new Readable;
 
-// Create the Speaker instance
+// params
+var A = 100; //amplitude
+
+
+
+
+// audio output to speaker
 var speaker = new Speaker({
-  channels: 2,          // 2 channels
-  bitDepth: 16,         // 16-bit samples
-  sampleRate: 44100     // 44,100 Hz sample rate
+    channels: 1,          // 2 channels
+    bitDepth: 16,         // 16-bit samples
+    sampleRate: 44100     // 44,100 Hz sample rate
 });
 
 
-// Create a screen object.
-var screen = blessed.screen({
-    smartCSR: true
-});
 
-screen.title = 'freqmod2';
+var screen = blessed.screen(),
+    body = blessed.box({
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        tags: true
+    });
 
-// Create a box perfectly centered horizontally and vertically.
-var box = blessed.box({
-    content: 'freqmod2',
-    tags: true,
-    border: {
-        type: 'line'
-    },
-    style: {
-        border: {
-            fg: '#f0f0f0'
-        }
-    }
-});
+screen.append(body);
 
-// Append our box to the screen.
-screen.append(box);
-
-// If box is focused, handle `enter`/`return` and give us some more content.
-screen.key('q', function(ch, key) { freq *= 2; });
-screen.key('w', function(ch, key) { freq /= 2; });
-
-screen.key('p', function(ch) {
-    rs.pipe(speaker);
-});
-
-// Quit on Escape, q, or Control-C.
-screen.key(['escape', 'q', 'C-c'], function(ch, key) {
+screen.key(['escape', 'q', 'C-c'], function() {
     return process.exit(0);
 });
 
-// Focus our element.
-box.focus();
-
-// Render the screen.
-screen.render();
-
-function fillBuf(i) {
-    var buflen = 1024;
-    var A = 2000;
-
-    var buf = new Uint16Array(buflen);
-    for(var j = 0; j < buflen; j++) {
-        buf[j] = Math.sin((i+j)/40)*A;
-    }
-    rs.push(buf.toString());
-    setTimeout(fillBuf, 10, i+j);
+function status(text) {
+    body.setLine(0, '{blue-fg}' + text + '{/blue-bg}');
+    screen.render();
+}
+function log(text) {
+    body.insertLine(1, text);
+    screen.render();
 }
 
-fillBuf(0);
+function Source(content, options) {
+    Readable.call(this, options);
+    this.content = content;
+}
+
+inherits(Source, Readable);
+
+var i = 0;
+Source.prototype._read = function (size) {
+    status((new Date()).toISOString()+' '+size);
+    log('Writing '+size+' bytes');
+    var arr = new Uint16Array(size);
+    for(var j = 0; j < size; j++) {
+        arr[j] = A*Math.sin((i+j)/40);
+    }
+    var buf1 = Buffer.from(arr); // copies the buffer
+    //console.log(buf1);
+    this.push(buf1);
+    i+=size; 
+};
+
+var myFile = fs.createWriteStream('output.wav');
+var rs = new Source();
+rs.pipe(speaker);
+rs.pipe(myFile);
+
 
